611f95731018b31e963c1612c90aa3952ddf2ac9
裕正 石田 <dbyusei0429@icloud.com>
2022-10-21 09:43:55 +0900

    2行目をNew branch changedに変更

--- gittest.cpp
+++ gittest.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub world\n";
 
-    std::cout << "Hello New branch\n";
+    std::cout << "New branch changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
