2f3dbc284a72e1013eaade26e0f97a84119f49a0
yuuseiishida <115197644+yuuseiishida@users.noreply.github.com>
2022-11-18 09:37:53 +0900

    リモートリポジトリでコンフリクト変更

--- gittest.cpp
+++ gittest.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub world\n";
 
-    std::cout << "New branch changed\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
